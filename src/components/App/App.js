6efd2ec4f1bb70e61539f2db37332850b1acfbc2
Katie Saramutina <saramutina@gmail.com>
2020-10-30 16:44:05 +0300

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TableFunc from '../Table/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <TableFunc />
+    </div>
+  );
+}
+
+export default App;
