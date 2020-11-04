424dd7b4c6aea0f82ce0d23943bd562e906525b6
Katie Saramutina <saramutina@gmail.com>
2020-11-04 19:56:01 +0300

    Use generated data

--- src/backend/routes/objects.js
+++ src/backend/routes/objects.js
@@ -1,49 +1,49 @@
 const objectRoutes = (app, fs) => {
-    const dataPath = './data/objects.json';
+  const dataPath = './data/generatedData.json';
 
-    app.get('/api/objects', (req, res) => {
-        fs.readFile(dataPath, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
+  app.get('/api/objects', (req, res) => {
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+          if (err) {
+              throw err;
+          }
 
-            res.send(JSON.parse(data));
-        });
-    });
+          res.send(JSON.parse(data));
+      });
+  });
 
-    app.get('/api/objects/:sortBy/:order', (req, res) => {
-        fs.readFile(dataPath, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            const column = req.params.sortBy;
-            const order = req.params.order;
-            const objects = JSON.parse(data);
-            const compareFunc = (a, b) => {
-                if (order === 'ascending') {
-                  if (a[column] < b[column]) {
-                    return -1;
-                  }
-                  if (a[column] > b[column]) {
-                    return 1;
-                  }
-                  return 0;
+  app.get('/api/objects/:sortBy/:order', (req, res) => {
+      fs.readFile(dataPath, 'utf8', (err, data) => {
+          if (err) {
+              throw err;
+          }
+          const column = req.params.sortBy;
+          const order = req.params.order;
+          const objects = JSON.parse(data);
+          const compareFunc = (a, b) => {
+              if (order === 'ascending') {
+                if (a[column] < b[column]) {
+                  return -1;
                 }
-                if (order === 'descending') {
-                  if (a[column] < b[column]) {
-                    return 1;
-                  }
-                  if (a[column] > b[column]) {
-                    return -1;
-                  }
-                  return 0;
+                if (a[column] > b[column]) {
+                  return 1;
                 }
-              };
-            const sortedObjects = objects.sort(compareFunc);
-            
-            res.send(sortedObjects);
-        });
-    });
+                return 0;
+              }
+              if (order === 'descending') {
+                if (a[column] < b[column]) {
+                  return 1;
+                }
+                if (a[column] > b[column]) {
+                  return -1;
+                }
+                return 0;
+              }
+            };
+          const sortedObjects = objects.sort(compareFunc);
+          
+          res.send(sortedObjects);
+      });
+  });
 };
 
 module.exports = objectRoutes;
